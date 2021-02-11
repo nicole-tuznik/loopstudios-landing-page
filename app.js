72b862ec65d96fb6d441e4ddea3bcc9b04355030
nicole-tuznik <69260034+nicole-tuznik@users.noreply.github.com>
2021-02-11 15:57:43 -0500

    Finished JavaScript, refined CSS styling

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const menu = document.querySelector('.nav__menu');
 const links = document.querySelector('.nav__links');
-let li = document.querySelectorAll('.nav__li');
 
 menu.addEventListener('click', function() {
-  // for (let i = 0; i < li.length; i++) {
-    
-  //   li[i].classList.add('active');
-  //   // li[i].style.display = 'block';
-    
-  // }
-
-  links.classList.add('active');
+  links.classList.toggle('active');
+  
+  if(links.classList.contains('active')) {
+    menu.src = '/images/icon-close.svg';
+  } else {
+    menu.src = '/images/icon-hamburger.svg';
+  };
 })
